842c84999cfb5b6c65d7b2b3429f93345afa642b
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-03 08:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let num1;
+num1 = prompt("Enter the first number:");
+			num1 = parseFloat(num1);
+			let result = num1 * 2;
+			let num2;// Get the input for the second variable using the prompt() function
+			num2 = prompt("Enter the second number:");
+			num2 = parseFloat(num2);
+				result += num2;
+			alert("The final result is: " + result);
